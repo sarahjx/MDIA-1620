e48aed5dbced4a0d7347afed20043270aae384f9
Sarah Wang <sarahwang@Sarahs-MacBook-Pro.local>
2024-09-20 15:33:57 -0700

    initialize stables

---
+++ stables.js
@@ -0,0 +1,8 @@
+let horseName = "blargle";
+console.log(horseName);
+horseName = "glarble";
+console.log(horseName);
+let horseAge = 6900;
+console.log(horseAge);
+let isHorseInside = true;
+console.log(isHorseInside);
