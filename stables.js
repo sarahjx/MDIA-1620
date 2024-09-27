c35deee92d313a8e47b8bf94a81b7338f922c915
Sarah <sarahjxwang@gmail.com>
2024-09-27 15:56:13 -0700

    Added Lesson 4 Lab

--- stables.js
+++ stables.js
@@ -23,4 +23,10 @@ console.log(2 + "2"); // also type coercion, becomes 22
 console.log(2 - "2"); // does math...
 console.log(`my horse babies names are ${horseName} and ${horseNameTwo } they are both ${horseAge } years old i love them`);
 console.log(`if you want to purchase a spot in my stable, the fee is ${STABLE_MONTHLY_FEE} carrots. peeled and cut into squares.`)
-console.log("69");
+
+// LAB 4 Work
+let horseNickname = "blargie baby"
+let discount = STABLE_MONTHLY_FEE * 3 *.10;
+
+console.log(`my horse's name is ${horseName} and his nickname is ${horseNickname}`);
+console.log(`you can save $${discount} if you stay for 3 months`)
