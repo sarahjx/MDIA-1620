94e7032d56d57685b5a9ccb0f74a8b55f8777dd1
Sarah Wang <sarahwang@Sarahs-MacBook-Pro.local>
2024-09-27 15:21:04 -0700

    Adding info to stables

--- stables.js
+++ stables.js
@@ -1,8 +1,25 @@
+// LESSON-3 Code
 let horseName = "blargle";
 console.log(horseName);
-horseName = "glarble";
-console.log(horseName);
+horseNameTwo = "glarble";
+console.log(horseNameTwo);
 let horseAge = 6900;
 console.log(horseAge);
 let isHorseInside = true;
 console.log(isHorseInside);
+
+// LESSON-4 Code
+console.log(typeof horseName);
+console.log(typeof horseAge);
+const STABLE_MONTHLY_FEE = 1000; // uppercase is preferred for constants
+console.log(STABLE_MONTHLY_FEE); 
+
+console.log(STABLE_MONTHLY_FEE * 3);
+console.log(STABLE_MONTHLY_FEE - 49);
+console.log(44/87);
+console.log("skibidi" + " toilet");
+console.log("four" + 20); // example of type coercion 
+console.log(2 + "2"); // also type coercion, becomes 22
+console.log(2 - "2"); // does math...
+console.log(`my horse babies names are ${horseName} and ${horseNameTwo } they are both ${horseAge } years old i love them`);
+console.log(`if you want to purchase a spot in my stable, the fee is ${STABLE_MONTHLY_FEE} carrots. peeled and cut into squares.`)
